0e65cd33648078e2c722b58dfedb84a5ce66295d
PENDO AGNES <99383447+pendoagnes222@users.noreply.github.com>
2022-11-01 09:28:25 +0300

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
-# install_a_package.pp
-# package
-
-package { 'flask-pip3':
-  ensure   => '2.1.1',
-  provider => 'gem'
+# Using Puppet, install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
+Footer
+
