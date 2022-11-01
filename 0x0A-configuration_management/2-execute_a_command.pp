fbab51b0d0df6a28388af121dcef6ba0f46691ce
PENDO AGNES <99383447+pendoagnes222@users.noreply.github.com>
2022-11-01 09:25:13 +0300

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,4 @@
-# kills a reunning server program
-
-exec { 'killmenow':
-  command => 'pkill -n killmenow',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => "test $(pgrep killmenow|wc -l) -ge 1",
+# Using Puppet, create a manifest that kills a process named killmenow
+exec { 'pkill -f killmenow':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
